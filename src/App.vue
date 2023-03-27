b40e8505490dafb344cfeadc735c60a8be1e0493
leonardoserra <serra.leonardo92@gmail.com>
2023-03-27 15:53:06 +0200

    creato scheletro codice, messa favicon, add scss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+import Banner from "./components/Banner.vue";
+
+<script>
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+    Banner
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+
+
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <TheHeader />
+  <main>
+
+  </main>
+  <TheFooter />
+  <Banner />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style lang="scss"></style>
