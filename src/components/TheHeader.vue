4248f5d36845cd9882d82da33b2e8035bb28292b
leonardoserra <serra.leonardo92@gmail.com>
2023-03-27 16:01:00 +0200

    creo array di oggettinell header per le categorie

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,41 @@
 <script>
 export default {
+    data() {
+        return {
+            navbarSections: [
+                {
+                    category: 'CHARACTERS'
+                },
+                {
+                    category: 'COMICS'
+                },
+                {
+                    category: 'MOVIES'
+                },
+                {
+                    category: 'TV'
+                },
+                {
+                    category: 'GAMES'
+                },
+                {
+                    category: 'COLLECTIBLES'
+                },
+                {
+                    category: 'VIDEOS'
+                },
+                {
+                    category: 'FANS'
+                },
+                {
+                    category: 'NEWS'
+                },
+                {
+                    category: 'SHOP'
+                }
+            ]
+        }
+    }
 
 }
 </script>
