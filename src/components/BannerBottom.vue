75e52e567fed93b0215376fb71a2d4bd61db416c
leonardoserra <serra.leonardo92@gmail.com>
2023-03-28 15:36:06 +0200

    creato jumbotron

--- src/components/BannerBottom.vue
+++ src/components/BannerBottom.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-    name: "Banner",
+    name: "BannerBottom",
     data() {
         return {
             icons: [
---
+++ src/components/TheJumbotron.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "TheJumbotron"
+}
+</script>
+
+
+<template>
+    <div class="background">
+    </div>
+</template>
+
+
+<style scoped lang="scss">
+.background {
+    height: 300px;
+    background-image: url(../assets/img/jumbotron.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
