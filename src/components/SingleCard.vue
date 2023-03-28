7ba9ef15c195a9226b549cb01b804defa9c049a5
leonardoserra <serra.leonardo92@gmail.com>
2023-03-28 15:47:52 +0200

    creo sezione card e creo elemento card con props

---
+++ src/components/SingleCard.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "SingleCard",
+    props: {
+        thumb: String,
+        series: String
+    }
+}
+
+</script>
+
+<template>
+    <div class="wrapper">
+
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
